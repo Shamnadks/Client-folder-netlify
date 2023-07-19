8a95997db9e94cc2c021e4ed97b89f5f4229a48d
Shamnadks <shamnadkskannur@gmail.com>
2023-07-19 19:48:54 +0530

    react-dom/s

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import authReducer from "./state";
@@ -28,7 +28,7 @@ const store = configureStore({
 
 
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <Provider store={store}>
