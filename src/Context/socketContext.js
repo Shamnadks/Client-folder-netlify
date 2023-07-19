43e63ed777101296d471af058ac4dae9d199fe9a
Shamnadks <shamnadkskannur@gmail.com>
2023-07-19 19:16:48 +0530

    httpss

--- src/Context/socketContext.js
+++ src/Context/socketContext.js
@@ -1,4 +1,4 @@
 import {createContext} from "react";
 import { io } from "socket.io-client";
-export const socket = io('http://shoemart.site') //'http://localhost:3001'
+export const socket = io('https://shoemart.site') //'http://localhost:3001'
 export const SocketContext = createContext(); 
